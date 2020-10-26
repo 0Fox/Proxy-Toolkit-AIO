7a8b428e48b7217057963e338a85be9c43ef0549
0Fox <Zimon4eR@yandex.ru>
2020-10-26 13:26:04 +0200

    fixed some overcomplicated string formatting

--- Proxy-Toolkit-AIO/Form1.cs
+++ Proxy-Toolkit-AIO/Form1.cs
@@ -63,7 +63,7 @@ namespace CS_Proxy {
 
             if ( !HasInternet() ) {
                 statusLbl.Text = "Status: No Internet";
-                MessageBox.Show( string.Concat( "You do NOT have access to the internet.", Environment.NewLine, "Press 'OK' to Terminate." ), "No Internet", MessageBoxButtons.OK, MessageBoxIcon.Warning );
+                MessageBox.Show( "You do NOT have access to the internet.\nPress 'OK' to Terminate.", "No Internet", MessageBoxButtons.OK, MessageBoxIcon.Warning );
                 Application.Exit();
             }
 
@@ -85,7 +85,7 @@ namespace CS_Proxy {
             scrapedPanel.Visible = ProxiesScraped.Count > 0;
             scrapeBtn.Enabled = URLMgr.Count > 0;
 
-            urlsToScrapeLbl.Text = string.Format( "URLs To Scrape: {0}", URLMgr.Count.ToString() );
+            urlsToScrapeLbl.Text = $"URLs To Scrape: {URLMgr.Count}";
             urlsToScrapeLbl.Location = new Point( importURLsBtn.Location.X - urlsToScrapeLbl.Width - 15, urlsToScrapeLbl.Location.Y );
         }
 
@@ -114,8 +114,8 @@ namespace CS_Proxy {
                 //Update UI
                 ScapePanelUI();
 
-                MessageBox.Show( string.Format( "A total of {0} URLs have been imported for scraping.{1}", URLMgr.Count.ToString(),
-                    badUrls > 0 ? string.Format( "{0}{1} URLs seem to be invalid and were thus omitted.", Environment.NewLine, badUrls.ToString() ) : "" ),
+                MessageBox.Show( string.Concat( $"A total of {URLMgr.Count} URLs have been imported for scraping.",
+                    badUrls > 0 ? $"\n{badUrls} URLs seem to be invalid and were thus omitted." : string.Empty ),
                     "Import URLs", MessageBoxButtons.OK, MessageBoxIcon.Information );
             }
         }
@@ -124,14 +124,14 @@ namespace CS_Proxy {
         private void UpdateScraperUI() {
             lock ( scrapeUILock ) {
                 if ( Scraper.TerminateThreads )
-                    scrapeBtn.Invoke( new Action( () => scrapeBtn.Text = string.Concat( "Threads = ", Scraper.Threads.ToString() ) ) );
+                    scrapeBtn.Invoke( new Action( () => scrapeBtn.Text = $"Threads = {Scraper.Threads}" ) );
 
                 // scrapedListBox.Invoke(new Action(() => scrapedListBox.SelectedIndex = scrapedListBox.Items.Count - 1)); //force scroll down
-                scrapedLbl.Invoke( new Action( () => scrapedLbl.Text = string.Format( "Scraped: {0}", ProxiesScraped.Count.ToString() ) ) ); //update label
+                scrapedLbl.Invoke( new Action( () => scrapedLbl.Text = $"Scraped: {ProxiesScraped.Count}" ) ); //update label
                 scrapeProgBar.Invoke( new Action( () => scrapeProgBar.Value = Scraper.URLScraped ) ); //update progress bar
 
                 if ( statusStrip1.Parent.InvokeRequired ) {
-                    var statuslbl = string.Format( "{0} out of {1} URLs Scraped!", Scraper.URLScraped.ToString(), URLMgr.Count.ToString() );
+                    var statuslbl = $"{Scraper.URLScraped} out of {URLMgr.Count} URLs Scraped!";
                     statusStrip1.Parent.Invoke( new MethodInvoker( delegate { statusLbl.Text = statuslbl; } ) );
                 }
             }
@@ -141,7 +141,7 @@ namespace CS_Proxy {
         private void ClearScrapeList() {
             ProxiesScraped.Clear();
             scrapedListBox.Items.Clear();
-            scrapedLbl.Text = string.Format( "Scraped: {0}", ProxiesScraped.Count.ToString() );
+            scrapedLbl.Text = $"Scraped: {ProxiesScraped.Count}";
             ScapePanelUI();
         }
         private void ClearScrapedBtn_Click(object sender, EventArgs e) {
@@ -192,7 +192,7 @@ namespace CS_Proxy {
                 var scraper = new Scraper( URLMgr, Timeout );
                 var thread = new Thread( new ThreadStart( scraper.GetProxies ) );
                 thread.IsBackground = true; //imp so all auto terminate on formClosing()
-                thread.Name = string.Concat( "Scraper_#", t.ToString() );
+                thread.Name = $"Scraper_#{t}";
                 threads[t] = thread;
                 Thread.Sleep( 10 );
             }
@@ -240,19 +240,18 @@ namespace CS_Proxy {
         private void ToScannerBtn_Click(object sender, EventArgs e) {
             ScrapedToScanner();
             if ( scrapedListBox.Items.Count > ProxyMgr.Count )
-                MessageBox.Show( string.Concat( (scrapedListBox.Items.Count - ProxyMgr.Count).ToString(), " DANGEROUS Proxies were removed." ), "Filter", MessageBoxButtons.OK, MessageBoxIcon.Information );
+                MessageBox.Show( $"{scrapedListBox.Items.Count - ProxyMgr.Count} DANGEROUS Proxies were removed.", "Filter", MessageBoxButtons.OK, MessageBoxIcon.Information );
             else
                 System.Media.SystemSounds.Beep.Play();
         }
 
         private void EnableScanUI() {
-            proxiesToScanLbl.Invoke( new Action( () => proxiesToScanLbl.Text = string.Format( "Proxies to Scan: {0}", ProxyMgr.Count.ToString() ) ) );
+            proxiesToScanLbl.Invoke( new Action( () => proxiesToScanLbl.Text = $"Proxies to Scan: {ProxyMgr.Count}" ) );
             using ( Graphics g = CreateGraphics() ) {
                 SizeF lblsize = g.MeasureString( proxiesToScanLbl.Text, proxiesToScanLbl.Font, 1000 );
                 var loc = new Point( ((importProxiesBtn.Width - Convert.ToInt32( lblsize.Width )) / 2) + importProxiesBtn.Location.X, proxiesToScanLbl.Location.Y );
                 proxiesToScanLbl.Invoke( new Action( () => proxiesToScanLbl.Location = loc ) );
             }
-            //proxiesToScanLbl.Location = new Point(importProxiesBtn.Location.X - proxiesToScanLbl.Width - 10, proxiesToScanLbl.Location.Y);
             scanProxiesBtn.Invoke( new Action( () => scanProxiesBtn.Enabled = ProxyMgr.Count > 0 ) );
         }
 
@@ -292,10 +291,9 @@ namespace CS_Proxy {
                 //Update UI
                 EnableScanUI();
 
-                MessageBox.Show( string.Format( "A total of {0} Proxies have been imported for scanning.{1}{2}", ProxyMgr.Count.ToString(),
-                    (badProxies + duplicateProxies) > 0 ? string.Format( "{0} - {1} bad proxies and {2} duplicates were removed!",
-                    Environment.NewLine, badProxies.ToString(), duplicateProxies.ToString() ) : "", dangerousProxies > 0 ? string.Concat( Environment.NewLine,
-                    " - ", dangerousProxies.ToString(), " DANGEROUS Proxies were also removed!" ) : "" ),
+                MessageBox.Show( string.Concat( $"A total of {ProxyMgr.Count} Proxies have been imported for scanning.",
+                    (badProxies + duplicateProxies) > 0 ? $"\n - {badProxies} bad proxies and {duplicateProxies} duplicates were removed!" : string.Empty,
+                    dangerousProxies > 0 ? $"\n - {dangerousProxies} DANGEROUS Proxies were also removed!" : string.Empty ),
                     "Import Proxies", MessageBoxButtons.OK, MessageBoxIcon.Information );
             }
         }
@@ -354,7 +352,7 @@ namespace CS_Proxy {
                 var scanner = new Scanner( ProxyMgr );
                 var thread = new Thread( new ThreadStart( scanner.Start ) );
                 thread.IsBackground = true; //imp so all auto terminate on formClosing()
-                thread.Name = string.Concat( "Scanner_#", t.ToString() );
+                thread.Name = $"Scanner_#{t}";
                 threads[t] = thread;
                 Thread.Sleep( 10 );
             }
@@ -419,7 +417,7 @@ namespace CS_Proxy {
         }
 
         private void QuestionScraperBtn_Click(object sender, EventArgs e) {
-            MessageBox.Show( string.Format( "This tab is used to scrape proxies off a given list of URLs.{0}Just hit the 'Import URLs' button and click the 'Scrape' button!", Environment.NewLine ),
+            MessageBox.Show( "This tab is used to scrape proxies off a given list of URLs.\nJust hit the 'Import URLs' button and click the 'Scrape' button!",
                 "?", MessageBoxButtons.OK, MessageBoxIcon.Information );
         }
 
@@ -459,7 +457,7 @@ namespace CS_Proxy {
         public void ReportHarvest() {
             harvestBtn.Invoke( new Action( () => harvestBtn.Enabled = true ) );
 
-            MessageBox.Show( string.Format( "URL harvesting has completed!{0}Obtained a total of {1} URLs.", Environment.NewLine, harvestBox.Items.Count.ToString() ),
+            MessageBox.Show( $"URL harvesting has completed!\nObtained a total of {harvestBox.Items.Count} URLs.",
                 "Harvesting Complete", MessageBoxButtons.OK, MessageBoxIcon.Information );
             queriesLeftLbl.Invoke( new Action( () => queriesLeftLbl.Text = "" ) );
 
@@ -471,9 +469,8 @@ namespace CS_Proxy {
         }
 
         private void UpdateHarvestUI(int queryNo, int totalQueries, int pageNo, string query) {
-            queriesLeftLbl.Invoke( new Action( () => queriesLeftLbl.Text = string.Concat( "Queries: ", queryNo.ToString(), "/", totalQueries.ToString(), Environment.NewLine,
-                  "Page: ", pageNo.ToString() ) ) );
-            urlsHarvestedLbl.Invoke( new Action( () => urlsHarvestedLbl.Text = string.Concat( "URLs: ", harvestBox.Items.Count.ToString() ) ) );
+            queriesLeftLbl.Invoke( new Action( () => queriesLeftLbl.Text = $"Queries: {queryNo}/{totalQueries}\nPage: {pageNo}" ) );
+            urlsHarvestedLbl.Invoke( new Action( () => urlsHarvestedLbl.Text = $"URLs: {harvestBox.Items.Count}" ) );
 
             if ( statusStrip3.Parent.InvokeRequired ) {
                 statusStrip3.Parent.Invoke( new MethodInvoker( delegate { harvestProgBar.Value = queryNo; } ) );
@@ -504,8 +501,7 @@ namespace CS_Proxy {
 
                 scrapeBtn.Invoke( new Action( () => scrapeBtn.Enabled = true ) );
                 if ( !autoScanCheck.Checked ) {
-                    MessageBox.Show( string.Format( "Scraping Finished!!!{0}Scraped a total of {1} proxies!!!{2}{3} were invalid/errorsome URLs and {4} contained no proxies...",
-                        Environment.NewLine, ProxiesScraped.Count.ToString(), Environment.NewLine, Scraper.BadURLs.ToString(), Scraper.EmptyURLs.ToString() ),
+                    MessageBox.Show( $"Scraping Finished!!!\nScraped a total of {ProxiesScraped.Count} proxies!!!\n{Scraper.BadURLs} were invalid/errorsome URLs and {Scraper.EmptyURLs} contained no proxies...",
                         "Scraper", MessageBoxButtons.OK, MessageBoxIcon.Information );
                 } else {
                     //AUTO-SCAN
@@ -571,21 +567,21 @@ namespace CS_Proxy {
 
         private Task UpdateScannerUI() { //This is updated periodically (on separate thread/timer) rather than potentially hundreds of threads calling this
             if ( Scanner.TerminateThreads )
-                scanProxiesBtn.Invoke( new Action( () => scanProxiesBtn.Text = string.Concat( "Threads = ", Scanner.Threads.ToString() ) ) );
+                scanProxiesBtn.Invoke( new Action( () => scanProxiesBtn.Text = $"Threads = {Scanner.Threads}" ) );
 
-            aliveLbl.Invoke( new Action( () => aliveLbl.Text = string.Concat( "Alive = ", Scanner.Alive.ToString(), "    |    Anonymous = ", (Scanner.High + Scanner.Elite).ToString() ) ) );
-            deadLbl.Invoke( new Action( () => deadLbl.Text = string.Concat( "Dead = ", Scanner.Dead.ToString() ) ) );
+            aliveLbl.Invoke( new Action( () => aliveLbl.Text = $"Alive = {Scanner.Alive}    |    Anonymous = {Scanner.High + Scanner.Elite}" ) );
+            deadLbl.Invoke( new Action( () => deadLbl.Text = $"Dead = {Scanner.Dead}" ) );
 
             if ( statusStrip2.Parent.InvokeRequired ) {
                 statusStrip2.Parent.Invoke( new MethodInvoker( delegate { scanProgBar.Value = Scanner.Scanned; } ) );
                 if ( Scanner.Scanned != 0 ) {
-                    var percent = ((float) Scanner.Scanned * 100.0f) / (float) ProxyMgr.Count;
+                    var percent = (Scanner.Scanned * 100.0f) / ProxyMgr.Count;
 
                     var seconds = (DateTime.Now - ScanStart).TotalSeconds; //use double coz of DivByZero Exc
                     var leftToScan = ProxyMgr.Count - Scanner.Scanned;
-                    var secondsLeft = ((double) leftToScan * seconds) / (double) Scanner.Scanned;
+                    var secondsLeft = (leftToScan * seconds) / Scanner.Scanned;
                     var ts = TimeSpan.FromSeconds( secondsLeft );
-                    statusStrip2.Parent.Invoke( new MethodInvoker( delegate { scanPercentLbl.Text = string.Concat( "ETA: ", ts.ToString( @"hh\:mm\:ss" ), "  [", percent.ToString( "0.00" ), "%]" ); } ) );
+                    statusStrip2.Parent.Invoke( new MethodInvoker( delegate { scanPercentLbl.Text = $"ETA: {ts:hh\\:mm\\:ss}  [{percent:0.00}%]"; } ) );
                 }
             }
 
@@ -596,9 +592,9 @@ namespace CS_Proxy {
                 threadsProxiesNumUpDown.Invoke( new Action( () => threadsProxiesNumUpDown.Enabled = true ) );
                 timeoutProxiesNumUpDown.Invoke( new Action( () => timeoutProxiesNumUpDown.Enabled = true ) );
 
-                MessageBox.Show( string.Format( "Found {0} working proxies!{1}{2}", Scanner.Alive.ToString(),
-                    Scanner.Https > 0 ? string.Concat( Environment.NewLine, "HTTP working = ", Scanner.Https.ToString() ) : "",
-                    Scanner.Socks > 0 ? string.Concat( Environment.NewLine, "SOCKS working = ", Scanner.Socks.ToString() ) : "" ),
+                MessageBox.Show( string.Concat( $"Found {Scanner.Alive} working proxies!",
+                    Scanner.Https > 0 ? $"\nHTTP working = {Scanner.Https}" : string.Empty,
+                    Scanner.Socks > 0 ? $"\nSOCKS working = {Scanner.Socks}" : string.Empty ),
                     "Scanner", MessageBoxButtons.OK, MessageBoxIcon.Information );
 
                 if ( statusStrip2.Parent.InvokeRequired ) {
@@ -644,13 +640,8 @@ namespace CS_Proxy {
 
         private void LinkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e) {
             DateTime today = DateTime.Now;
-            MessageBox.Show( string.Concat( "{d} = ", today.ToString( "d" ), Environment.NewLine,
-            "{dd} = ", today.ToString( "dd" ), Environment.NewLine,
-            "{mm} = ", today.ToString( "MM" ), Environment.NewLine,
-            "{mmm} = ", today.ToString( "MMM" ), Environment.NewLine,
-            "{mmmm} = ", today.ToString( "MMMM" ), Environment.NewLine,
-            "{yy} = ", today.ToString( "yy" ), Environment.NewLine,
-            "{yyyy} = ", today.ToString( "yyyy" ) ), "Operators", MessageBoxButtons.OK, MessageBoxIcon.Information );
+            MessageBox.Show( $"{{d}} = {today:d}\n{{dd}} = {today:dd}\n{{mm}} = {today:MM}\n{{mmm}} = {today:MMM}\n{{mmmm}} = {today:MMMM}\n{{yy}} = {today:yy}\n{{yyyy}} = {today:yyyy}",
+                "Operators", MessageBoxButtons.OK, MessageBoxIcon.Information );
         }
 
         private void LinkLabel5_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e) {
